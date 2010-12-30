a208c0a191d9762d6fabc72552ec935d00be55e1
stephenmcd <steve@jupo.org>
2010-12-31 07:23:57 +1100

    Refactored server into a stoppable thread class.

--- chat.py
+++ chat.py
@@ -3,128 +3,181 @@
 import optparse
 import socket
 import thread
+import threading
 import time
 
 
-def accept(conn):
+class StoppableThread(threading.Thread):
     """
-    Call the inner func in a thread so as not to block. Wait for a 
-    name to be entered from the given connection. Once a name is 
-    entered, set the connection to non-blocking and add the user to 
-    the users dict.
+    Thread class that runs a main() method until the stop() method 
+    is called.
     """
-    def threaded():
+    
+    def stop(self):
+        """
+        Stops the main event loop.
+        """
+        self.running = False
+
+    def main(self):
+        """
+        Main method to be overridden by subclass that runs each 
+        event loop iteration.
+        """
+        pass
+
+    def end(self):
+        """
+        Called once the thread had been stopped and the final 
+        iteration of main() has run.
+        """
+        pass
+        
+    def run(self):
+        """
+        Run main method until stop() is called.
+        """
+        self.running = True
+        while self.running:
+            self.main()
+            time.sleep(.1)
+        self.end()
+
+class Server(StoppableThread):
+    """
+    Chat server.
+    """
+    
+    def __init__(self, host, port):
+        """
+        Set up the server socket.
+        """
+        self.commands = {
+            "!quit": "quit",
+            "!users": "list_users",
+            "!commands": "list_commands",
+        }
+        self.users = {}
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.setblocking(False)
+        self.server.bind((host, port))
+        self.server.listen(1)
+        print "Listening on %s:%s" % self.server.getsockname()
+        super(Server, self).__init__()
+
+    def main(self):
+        """
+        Main event loop iteration.
+        """
+        # Accept new connections.
         while True:
-            conn.send("Please enter your name: ")
             try:
-                name = conn.recv(1024).strip()
+                conn, addr = self.server.accept()
+            except socket.error:
+                break
+            self.accept(conn)
+        # Read from connections.
+        for name, conn in self.users.items():
+            try:
+                message = conn.recv(1024)
             except socket.error:
                 continue
-            if name in users.keys():
-                conn.send("Name entered is already in use.\n")
-            elif name:
-                conn.setblocking(False)
-                users[name] = conn
-                conn.send("Welcome %s!\n" % name)
-                list_commands(conn)
-                list_users(conn)
-                broadcast(name, action="joins")
-                break
-    thread.start_new_thread(threaded, ())
-
-
-def broadcast(name="", message=None, action=None):
-    """
-    Send a message to all users from the given name. If no name specified, 
-    the message is from the server. If no message is specified and an action 
-    is given, use the action as the message without a colon prefix to 
-    signify it isn't a message.
-    """
-    timestamp = time.strftime("[%I:%M:%S]")
-    if message is None:
-        message = "%s %s %s" % (timestamp, name, action)
-    else:
-        message = "%s %s: %s" % (timestamp, name, message)
-    for to_name, conn in users.items():
-        if to_name != name:
+            if not message:
+                # Empty string is given on disconnect,
+                # close connection.
+                conn.close()
+            else:
+                # Handle command if given, otherwise 
+                # broadcast message.
+                message = message.strip()
+                try:
+                    command = self.commands[message]
+                except KeyError:
+                    self.broadcast(name, message)
+                else:
+                    getattr(self, command)(conn)
+            # Check for disconnected users and remove them.
             try:
-                conn.send(message + "\n")
+                conn.send("")
             except socket.error:
-                pass
-
-
-def quit(conn):
-    """
-    Disconnect a connection.
-    """
-    conn.close()
-
-
-def list_users(conn):
-    """
-    Send the list of users to a connection.
-    """
-    conn.send("Current users are: %s\n" % ", ".join(users.keys()))
-
-
-def list_commands(conn):
-    """
-    Send the list of commands to a connection.
-    """
-    conn.send("Available commands are: %s\n" % " ".join(commands.keys()))
-
-
-def serve(host, port):
-    """
-    Main function - set up server socket and run main event loop.
-    """
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.setblocking(False)
-    server.bind((host, port))
-    server.listen(1)
-    print "Listening on %s:%s" % server.getsockname()
-    # Main event loop.
-    while True:
-        try:
-            # Accept new connections.
+                del self.users[name]
+                self.broadcast(name, action="leaves")
+        time.sleep(.1)
+    
+    def end(self):
+        """
+        Send a shutdown message to all users and close their 
+        connections.
+        """
+        self.broadcast(action="shutting down")
+        for conn in self.users.values():
+            conn.close()
+            
+    def accept(self, conn):
+        """
+        Call the inner func in a thread so as not to block. Wait for a 
+        name to be entered from the given connection. Once a name is 
+        entered, set the connection to non-blocking and add the user to 
+        the users dict.
+        """
+        def threaded():
             while True:
+                conn.send("Please enter your name: ")
                 try:
-                    conn, addr = server.accept()
-                except socket.error:
-                    break
-                accept(conn)
-            # Read from connections.
-            for name, conn in users.items():
-                try:
-                    message = conn.recv(1024)
+                    name = conn.recv(1024).strip()
                 except socket.error:
                     continue
-                if not message:
-                    # Empty string is given on disconnect, close connection.
-                    conn.close()
-                else:
-                    # Handle command if given, otherwise broadcast message.
-                    message = message.strip()
-                    try:
-                        command = commands[message]
-                    except KeyError:
-                        broadcast(name, message)
-                    else:
-                        command(conn)
-                # Check for disconnected users and remove them.
+                if name in self.users.keys():
+                    conn.send("Name entered is already in use.\n")
+                elif name:
+                    conn.setblocking(False)
+                    self.users[name] = conn
+                    conn.send("Welcome %s!\n" % name)
+                    self.list_commands(conn)
+                    self.list_users(conn)
+                    self.broadcast(name, action="joins")
+                    break
+        thread.start_new_thread(threaded, ())
+
+    def broadcast(self, name="", message=None, action=None):
+        """
+        Send a message to all users from the given name. If no name specified, 
+        the message is from the server. If no message is specified and an action 
+        is given, use the action as the message without a colon prefix to 
+        signify it isn't a message.
+        """
+        timestamp = time.strftime("[%I:%M:%S]")
+        if message is None:
+            message = "%s %s %s" % (timestamp, name, action)
+        else:
+            message = "%s %s: %s" % (timestamp, name, message)
+        for to_name, conn in self.users.items():
+            if to_name != name:
                 try:
-                    conn.send("")
+                    conn.send(message + "\n")
                 except socket.error:
-                    del users[name]
-                    broadcast(name, action="leaves")
-            time.sleep(.1)
-        except (SystemExit, KeyboardInterrupt):
-            print "shutdown?"
-            broadcast(action="shutting down")
-            for conn in users.values():
-                conn.close()
-            break
+                    pass
+
+    def quit(self, conn):
+        """
+        Disconnect a connection.
+        """
+        conn.close()
+
+    def list_users(self, conn):
+        """
+        Send the list of users to a connection.
+        """
+        users = sorted(self.users.keys())
+        conn.send("Current users are: %s\n" % ", ".join(users))
+
+    def list_commands(self, conn):
+        """
+        Send the list of commands to a connection.
+        """
+        commands = sorted(self.commands.keys())
+        conn.send("Available commands are: %s\n" % " ".join(commands))
 
 
 def client(host, port, name):
@@ -167,20 +220,14 @@ def client(host, port, name):
                     print message
             except socket.error:
                 time.sleep(.1)
+            # Handle disconnection.
+            try:
+                client.send("")
+            except socket.error:
+                break
         except (SystemExit, KeyboardInterrupt):
-            client.close()
             break
-
-
-# user name/connection mapping for all users.
-users = {}
-
-# command name/handler mapping for commands.
-commands = {
-    "!quit": quit,
-    "!users": list_users,
-    "!commands": list_commands,
-}
+    client.close()
 
 
 if __name__ == "__main__":
@@ -197,6 +244,8 @@ if __name__ == "__main__":
     except ValueError:
         parser.error("Address not in the format host:port")
     # Run server.
-    thread.start_new_thread(serve, (host, port))
+    server = Server(host, port)
+    server.start()
     time.sleep(1)
     client(host, port, raw_input("Please enter your name: "))
+    server.stop()
